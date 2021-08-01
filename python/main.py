22600be110dbcda6e7237a95eec95cfa2456c9ce
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-01 16:07:14 -0700

    completed game function and added ascii art

--- python/main.py
+++ python/main.py
@@ -14,6 +14,7 @@ def compare_accounts(a,b):
 
 def play_game():
     accounts = []
+    score = 0
     game_over = False
 
     while not game_over:
@@ -25,9 +26,24 @@ def play_game():
                 accounts.append(account)
             elif account != accounts[0]:
                 accounts.append(account)
-
-        print(accounts[0]['name'])
-        print(accounts[1]['name'])
-        game_over = True
+        print(art.logo)
+        print(f"Your current score is {score}")
+        print(f"Choice A: {accounts[0]['name']}, a {accounts[0]['description']}, from {accounts[0]['country']}")
+        print(art.vs)
+        print(f"Choice B: {accounts[1]['name']}, a {accounts[1]['description']}, from {accounts[1]['country']}")
+        
+        guess = input("Who has more followers? A or B? ").upper()
+        
+        if guess == "A":
+            guess = compare_accounts(accounts[0],accounts[1])
+        elif guess == "B":
+            guess = compare_accounts(accounts[1],accounts[0])
+        
+        if guess:
+            score += 1
+            accounts.pop(0)
+        else:
+            game_over = True
+            print(f"Wrong! Your final score is {score}!")
 
 play_game()
